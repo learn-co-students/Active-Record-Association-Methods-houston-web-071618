eb405f0912581db5d2018fc2d57813268d45c02f
github username <github email address>
2018-07-29 12:11:56 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,14 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    genre.name
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    artist = Artist.new(name: "Drake")
+    artist.save
+    self.artist_id = artist.id
+    artist
   end
 end
